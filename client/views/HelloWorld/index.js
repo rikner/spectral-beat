27b08b68ec346a0e209f1cdb179bab281989ac1d
rikner <rikner@web.de>
2017-01-12 00:35:25 +0100

    delete hello world

--- client/views/HelloWorld/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class HelloWorld extends React.Component {
-  render() {
-    return (
-      <h1>Hello World</h1>
-    );
-  }
-}
--- client/views/SpectralBeatMainView/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class SpectralBeatMainView extends React.Component {
-  render() {
-    return (
-      <h1>SpectralBeat</h1>
-    );
-  }
-}
