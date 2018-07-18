77ec1af14ae052eac59ad101d89578ff45580c93
rikner <rikner@web.de>
2018-07-18 22:18:06 +0200

    overhaul OnsetGraph

--- src/components/OnsetGraph.js
+++ src/components/OnsetGraph.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const w = 512;
-const h = 600;
+const canvasWidth = 512;
+const canvasHeight = 600;
 const onsetScale = 100;
 
-const onsetValues = new Array(w);
-const thresholdValues = new Array(w);
-const peakValues = new Array(w);
+const onsetValues = new Array(canvasWidth);
+const thresholdValues = new Array(canvasWidth);
+const peakValues = new Array(canvasWidth);
 
 
 function updateCanvas(onsetData) {
     onsetValues.shift();
     onsetValues.push(onsetData.value);
-    
+
     thresholdValues.shift();
     thresholdValues.push(onsetData.threshold);
-    
+
     peakValues.shift();
     peakValues.push(onsetData.isPeak);
 }
 
 (function () {
-    for (let i = 0; i < w; i++) {
+    for (let i = 0; i < canvasWidth; i++) {
         onsetValues[i] = 0;
         thresholdValues[i] = 0;
         peakValues[i] = false;
@@ -46,61 +46,61 @@ class OnsetGraph extends Component {
     componentDidMount = () => {
         this.startLoop();
     };
-    
+
     componentWillUnmount = () => {
         this.stopLoop();
     };
-    
+
     componentWillUpdate = (nextProps) => {
         updateCanvas(nextProps.onsetData);
     }
-    
+
     startLoop = () => {
         if (!this.frameId) {
             this.frameId = window.requestAnimationFrame(this.loop);
         }
     }
-    
+
     loop = () => {
         this.drawCanvas();
         this.frameId = window.requestAnimationFrame(this.loop);
     }
-    
+
     stopLoop = () => {
         window.cancelAnimationFrame(this.frameId);
     }
-    
+
     drawCanvas = () => {
         const onsetCanvasCtx = this.canvas.getContext("2d");
-        onsetCanvasCtx.fillStyle = "black";
-        onsetCanvasCtx.fillRect(0, 0, w, h);
-        
-        onsetCanvasCtx.fillStyle = "pink";
-        for (let i = 0; i < peakValues.length; i++) {
-            if (peakValues[i] === true) {
-                onsetCanvasCtx.fillRect(i, h, 1, -onsetValues[i] * onsetScale);
-            }
-        }
-        
+        onsetCanvasCtx.fillStyle = "black"; // e.g. rgba(0, 0, 200, 0.5)
+        onsetCanvasCtx.fillRect(0, 0, canvasWidth, canvasHeight);
+
         onsetCanvasCtx.fillStyle = "blue";
-        for (let i = 0; i < thresholdValues.length; i++) {
-            onsetCanvasCtx.fillRect(i, h, 1, -thresholdValues[i] * onsetScale);
-        }
-        
+        thresholdValues.forEach((value, i) => {
+            onsetCanvasCtx.fillRect(i, canvasHeight, 1, -value * onsetScale);
+        })
+
         onsetCanvasCtx.fillStyle = "white";
-        for (let i = 0; i < onsetValues.length; i++) {
-            onsetCanvasCtx.fillRect(i, h, 1, -onsetValues[i] * onsetScale);
-        }
+        onsetValues.forEach((value, i) => {
+            onsetCanvasCtx.fillRect(i, canvasHeight, 1, -value * onsetScale);
+        })
+
+        onsetCanvasCtx.fillStyle = "grey";
+        peakValues.forEach((value, i) => {
+            if (value === true) {
+                onsetCanvasCtx.fillRect(i, canvasHeight, 1, -canvasHeight);
+            }
+        })
     }
-    
+
     render() {
         return (
             <canvas
-            ref={canvas => {
-                this.canvas = canvas;
-            }}
-            width={w}
-            height={h}
+                ref={canvas => {
+                    this.canvas = canvas;
+                }}
+                width={canvasWidth}
+                height={canvasHeight}
             />
         );
     }
