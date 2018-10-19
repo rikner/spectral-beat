e5e2ef212b017abe990d052e2c0d554a50804fd7
rikner <rikner@web.de>
2018-10-19 21:26:24 +0200

    auto resize canvas

--- src/components/SettingsOverlay.js
+++ src/components/SettingsOverlay.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import OnsetGraph from './OnsetGraph';
 import ThresholdController from './ThresholdController';
+import withSizes from 'react-sizes'
+
 
 class SettingsOverlay extends Component {
     render() {
+        const { windowWidth, windowHeight } = this.props;
+
         return (
             <div style={{
                 alignItems: 'center',
@@ -11,17 +16,27 @@ class SettingsOverlay extends Component {
                 flexDirection: 'column',
                 justifyContent: 'space-around',
                 position: 'relative',
-                top: '2em'
+                top: '2em',
             }}>
-                <OnsetGraph canvasHeight={300} canvasWidth={400}/>
+                <OnsetGraph
+                    canvasHeight={Math.round(windowHeight / 3)}
+                    canvasWidth={Math.round(windowWidth / 2)}
+                />
                 <ThresholdController />
-            </div >
+            </div>
         );
     }
 }
 
 SettingsOverlay.props = {
-
+    windowHeight: PropTypes.number.isRequired,
+    windowWidth: PropTypes.number.isRequired,
 };
 
-export default SettingsOverlay;
+const mapSizesToProps = ({ width, height }) => ({
+    windowHeight: height,
+    windowWidth: width,
+})
+
+
+export default withSizes(mapSizesToProps)(SettingsOverlay);
