63c55b7b10ff4e4593145b1b00b4edb2316e69f9
rikner <rikner@web.de>
2018-11-22 23:09:43 +0100

    kick crossbrowsergetusermedia, introduce connect and disconnect functions

--- src/lib/AudioEngine/WebAudioEngine.ts
+++ src/lib/AudioEngine/WebAudioEngine.ts
@@ -1,11 +1,9 @@
 class WebAudioEngine {
 	public onFloatFrequencyData: ((data: Float32Array) => void) | null = null;
-	
+
 	private audioContext: AudioContext;
-	
+
 	private inputNode: MediaStreamAudioSourceNode | null = null;
-	private micStream: MediaStream | null = null;
-	
 	private filterNode: BiquadFilterNode;
 	private analyserNode: AnalyserNode;
 	private processingNode: ScriptProcessorNode;
@@ -16,49 +14,55 @@ class WebAudioEngine {
 		const AudioContext = (window as any).AudioContext || (window as any).webkitAudioContext;
 		this.audioContext = new AudioContext();
 		this.bufferSize = bufferSize;
-		
-		crossBrowserGetUserUserMedia({
-			onError: console.error,
-			onSuccess: (mediaStream: MediaStream) => {
-				console.log("mediastream sunccess")
-				this.micStream = mediaStream;
-				this.inputNode = this.audioContext.createMediaStreamSource(this.micStream);
-			},
-			options: { audio: true },
-		});
-		
+
 		// filter
 		this.filterNode = this.audioContext.createBiquadFilter();
 		this.filterNode.type = "lowpass";
 		this.filterNode.frequency.setValueAtTime(1000, 0);
-		
+
 		// fft
 		this.analyserNode = this.audioContext.createAnalyser();
-		
+
 		// processing
 		this.processingNode = this.audioContext.createScriptProcessor(this.bufferSize, 1, 1);
 		this.processingNode.onaudioprocess = this.audioProcessingCallback;
-		
+
 		// gain
 		this.gainNode = this.audioContext.createGain();
 		this.gainNode.gain.setValueAtTime(0, this.audioContext.currentTime + 1);
 	}
-	
-	public get frequencyBinCount() : number {
+
+	public get frequencyBinCount(): number {
 		return this.analyserNode.frequencyBinCount;
-	}	
-	
+	}
+
 	public start(): void {
-		if (!this.inputNode) {
-			return;
+		if (this.inputNode) {
+			this.connect();
+		} else {
+			navigator.mediaDevices.getUserMedia({ audio: true })
+			.then((mediaStream: MediaStream) => {
+				this.inputNode = this.audioContext.createMediaStreamSource(mediaStream);
+				this.connect();
+			})
+			.catch(console.error);
 		}
-		this.inputNode.connect(this.analyserNode);
+	}
+
+	public stop(): void {
+		this.disconnect();
+	}
+
+	private connect(): void {
+		if (this.inputNode) {
+			this.inputNode.connect(this.analyserNode);
+		}
 		this.analyserNode.connect(this.processingNode);
 		this.processingNode.connect(this.gainNode);
 		this.gainNode.connect(this.audioContext.destination);
 	}
-	
-	public stop(): void {
+
+	private disconnect(): void {
 		if (this.inputNode) {
 			this.inputNode.disconnect();
 		}
@@ -67,11 +71,11 @@ class WebAudioEngine {
 		this.processingNode.disconnect();
 		this.gainNode.disconnect();
 	}
-	
+
 	private audioProcessingCallback = (audioProcessingEvent: AudioProcessingEvent) => {
 		const dataArray = new Float32Array(this.analyserNode.frequencyBinCount);
 		void this.analyserNode.getFloatTimeDomainData(dataArray);
-		console.log(dataArray);
+		// console.log(dataArray);
 
 		if (this.onFloatFrequencyData) {
 			this.onFloatFrequencyData(dataArray);
@@ -80,19 +84,3 @@ class WebAudioEngine {
 }
 
 export default WebAudioEngine;
-
-
-function crossBrowserGetUserUserMedia({ onSuccess, onError, options }: {
-	onSuccess: NavigatorUserMediaSuccessCallback, 
-	onError: NavigatorUserMediaErrorCallback,
-	options: MediaStreamConstraints
-}) : void {
-	if (typeof navigator.mediaDevices.getUserMedia === "function") {
-		console.log("using mediaDevices.getUsermedia")
-		navigator.mediaDevices.getUserMedia(options)
-		.then(onSuccess)
-		.catch(onError)
-	} else {
-		navigator.getUserMedia(options, onSuccess, onError)
-	}
-}
