71408a9f93d63fd3a46f977b819e245342214d97
rikner <rikner@web.de>
2018-11-27 00:18:04 +0100

    first working fullscreen button version; also changed controls position

--- src/views/SpectralBeatMainView.js
+++ src/views/SpectralBeatMainView.js
@@ -6,25 +6,62 @@ import SettingsOverlay from '../components/SettingsOverlay';
 import OnsetDetectionController from '../components/OnsetDetectionController';
 import * as actions from '../actions';
 
-const SpectralBeatMainView = ({ settingsAreVisible, backgroundColor }) =>
-    <div style={{ backgroundColor, ...styles.main }}>
-        <div style={styles.controllerContainer}>
-            <OnsetDetectionController />
-        </div>
-        <div style={styles.settingsContainer} >
-            {settingsAreVisible
-                ? <SettingsOverlay />
-                : null
-            }
-        </div>
-    </div>
+class SpectralBeatMainView extends Component {
+    constructor(props) {
+        super(props);
+        this.mainView = React.createRef();
+    }
+    render() {
+        const { settingsAreVisible, backgroundColor } =  this.props;
+        return (
+            <div 
+                ref={this.mainView}
+                style={{ backgroundColor, ...styles.main }}
+            >
+                <div style={styles.fullscreenButtonWrapper}>
+                    <img
+                        src={window.location.origin + '/fullscreen.png'}
+                        style={styles.fullscreenButton}
+                        onClick={() => {
+                            if (document.webkitIsFullScreen) {
+                                document.webkitExitFullscreen()
+                            } else {
+                                this.mainView.current.webkitRequestFullscreen();
+                            }
+                        }}
+                    />
+                </div>
 
+                <div style={styles.settingsContainer} >
+                    {settingsAreVisible
+                        ? <SettingsOverlay />
+                        : null
+                    }
+                </div>
+                <div style={styles.controllerContainer}>
+                    <OnsetDetectionController />
+                </div>
+
+            </div>
+        )
+    }
+}
 
 const styles = {
     controllerContainer: {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-start',
+    },
+    fullscreenButton: {
+        height: "3em",
+        width: "3em",
+    },
+    fullscreenButtonWrapper: {
+        // backgroundColor: 'red',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
     },
     main: {
         alignItems: 'stretch',
