b8bedde19504e6b412cdeb2064533af61de55b11
rikner <rikner@web.de>
2019-09-15 23:42:03 +0200

    Updates

---
+++ precache-manifest.bc8167f6af656ac5a3ee01ade540737a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f037c345313519772137",
+    "url": "/spectral-beat/static/js/runtime~main.f037c345.js"
+  },
+  {
+    "revision": "76fc42c3b2a4bf843dcc",
+    "url": "/spectral-beat/static/js/main.76fc42c3.chunk.js"
+  },
+  {
+    "revision": "6a5169b140de4c58737f",
+    "url": "/spectral-beat/static/js/1.6a5169b1.chunk.js"
+  },
+  {
+    "revision": "6a5169b140de4c58737f",
+    "url": "/spectral-beat/static/css/1.11bf7eb9.chunk.css"
+  },
+  {
+    "revision": "fb7f43376d89e2d99682ba4b1267c27d",
+    "url": "/spectral-beat/index.html"
+  }
+];
